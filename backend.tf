26d4dc2818f25f20b8073d3a07ead271c5d91218
sidfaizan <56643525+sidfaizan@users.noreply.github.com>
2026-06-25 15:24:42 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,13 +3,9 @@ terraform {
   backend "s3" {
 
     bucket = "my-terraform-state-bucket"
-
     key = "eks/terraform.tfstate"
-
     region = "us-east-1"
-
     use_lockfile = true
-
     encrypt = true
   }
 }
